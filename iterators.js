1a497003c05f01e909330c9184d2e787ad9c46c6
moryw <matt@skitskat.xyz>
2018-04-02 19:59:10 -0300

    Add forEach method to array.

---
+++ iterators.js
@@ -0,0 +1,56 @@
+// //cc iterators example code
+//
+// let artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
+//
+// artists.forEach(function(person) {
+//   console.log(person + ' is one of my favorite artists.');
+// });
+//
+// let numbers = [1, 2, 3, 4, 5];
+//
+// let squareNumbers = numbers.map(function(number) {
+//   return number * number;
+// });
+//
+// console.log(squareNumbers);
+//
+// let things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
+//
+// let onlyNumbers = things.filter(function(thing) {
+//   return typeof thing === 'number';
+// });
+//
+// console.log(onlyNumbers);
+
+
+//forEach iterator
+
+let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+fruits.forEach(fruit =>
+  console.log('I want to eat a ' + fruit));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//end js
