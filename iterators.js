c9e7a5f18c3235022805e5bbbcaeef61f938aad3
moryw <matt@skitskat.xyz>
2018-04-11 01:06:30 -0400

    Fix some broken iterator methods called on functions

--- iterators.js
+++ iterators.js
@@ -83,6 +83,27 @@
 // let longFavoriteWords = favoriteWords.filter(word => word.length > 7); //arrow function
 
 
+//fix broken methods with documentation
+let words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+
+// Something is missing in the method call below
+
+console.log(words.some(function(word) {
+  return word.length < 6;
+}));
+
+// Use filter to create a new array
+let interestingWords = words.filter(function(word) {
+  return word.length > 5;
+});
+
+let interestingWords = words.filter(word => word.length > 5);
+console.log(interestingWords);
+
+// Make sure to uncomment the code below and fix the incorrect code before running it
+
+console.log(interestingWords.every(word => word.length > 5));
+
 
 
 
