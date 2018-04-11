01a8109fd608e9f28ed1a2fa3db8dc37e45b0b1e
moryw <matt@skitskat.xyz>
2018-04-11 08:28:04 +0300

    Finish iterators section.

--- iterators.js
+++ iterators.js
@@ -106,22 +106,22 @@
 // console.log(interestingWords.every(word => word.length > 5));
 
 
-//choose correct iterator methods
-let cities = ['Nashville', 'Charlotte', 'Asheville', 'Austin', 'Boulder'];
-
-let nums = [1, 50, 75, 200, 350, 525, 1000];
-
-//  Choose a method that will return undefined
-cities.forEach(city => console.log('Have you visited ' + city + '?'));
-
-// Choose a method that will return a new array
-let longCities = cities.filter(city => city.length > 7);
-
-// Choose a method that will return a new array
-let smallerNums = nums.map(num => num - 5);
-
-// Choose a method that will return a boolean value
-nums.some(num => num < 0);
+// //choose correct iterator methods
+// let cities = ['Nashville', 'Charlotte', 'Asheville', 'Austin', 'Boulder'];
+//
+// let nums = [1, 50, 75, 200, 350, 525, 1000];
+//
+// //  Choose a method that will return undefined
+// cities.forEach(city => console.log('Have you visited ' + city + '?'));
+//
+// // Choose a method that will return a new array
+// let longCities = cities.filter(city => city.length > 7);
+//
+// // Choose a method that will return a new array
+// let smallerNums = nums.map(num => num - 5);
+//
+// // Choose a method that will return a boolean value
+// nums.some(num => num < 0);
 
 
 
